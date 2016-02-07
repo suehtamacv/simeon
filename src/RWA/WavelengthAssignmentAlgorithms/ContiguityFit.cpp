cdb2adf2ba2909ee071ffd13b8eeab13d7eeb18e
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2016-02-07 10:19:47 -0300

    Faster ContiguityFit

--- src/RWA/WavelengthAssignmentAlgorithms/ContiguityFit.cpp
+++ src/RWA/WavelengthAssignmentAlgorithms/ContiguityFit.cpp
@@ -20,13 +20,14 @@ std::map<std::weak_ptr<Link>,
         std::owner_less<std::weak_ptr<Link>>> Slots;
     Slots.clear();
 
-    bool SlotsAvailability[Link::NumSlots];
-    std::vector<int> SpectralHoleSize(1, 0);
+    bool SlotsAvailability[Link::NumSlots + 1];
+    std::set<std::pair<int, int>> InitialSlotsAndHoles;
 
     for (int i = 0; i < Link::NumSlots; i++)
         {
         SlotsAvailability[i] = true;
         }
+    SlotsAvailability[Link::NumSlots] = false;
 
     for (auto &link : Seg.Links)
         {
@@ -38,10 +39,9 @@ std::map<std::weak_ptr<Link>,
             }
         }
 
-    unsigned int CurrentFreeSlots = 0;
-    int si = -1, sf = 0;
+    int CurrentFreeSlots = 0;
 
-    for (int sf = 0; sf < Link::NumSlots; sf++)
+    for (int sf = 0; sf <= Link::NumSlots; sf++)
         {
         if (SlotsAvailability[sf])
             {
@@ -49,43 +49,22 @@ std::map<std::weak_ptr<Link>,
             }
         else
             {
+            if (CurrentFreeSlots != 0 && CurrentFreeSlots >= RequiredSlots)
+                {
+                InitialSlotsAndHoles.emplace(CurrentFreeSlots, sf - CurrentFreeSlots);
+                }
             CurrentFreeSlots = 0;
             }
-        SpectralHoleSize.push_back(CurrentFreeSlots);
         }
 
-    SpectralHoleSize.push_back(0);
-    //Forms a sequence of the form 0 1 2 0 1 2 3 0 1 0 0, where the first and last
-    //zeros are always there, and serve to easy the search of a objective subsequence.
-    //The objective is to find a subsequence of the form 0 1 2 ... NRequiredSlots 0,
-    //that is, a hole with exactly NRequiredSlots. If such hole doesn't exists, then
-    //holes bigger and bigger are searched
-
-    for (int holeSize = RequiredSlots; holeSize <= Link::NumSlots; ++holeSize)
+    if (!InitialSlotsAndHoles.empty())
         {
-        std::vector<int> Hole(holeSize + 2, 0);
-        std::iota(Hole.begin() + 1, Hole.end() - 1, 1);
-
-        auto SFIterator = std::search(SpectralHoleSize.begin(), SpectralHoleSize.end(),
-                                      Hole.begin(), Hole.end());
-
-        if (SFIterator != SpectralHoleSize.end())
-            {
-            si = (SFIterator - SpectralHoleSize.begin());
-            sf = si + RequiredSlots - 1;
-            break;
-            }
-        }
-
-    if (si != -1)
-        {
-        BOOST_ASSERT_MSG(sf == si + RequiredSlots - 1,
-                         "Error in Wavelength Assingment");
+        int si = InitialSlotsAndHoles.begin()->second;
         for (auto &link : Seg.Links)
             {
             Slots.emplace(link, std::vector<std::weak_ptr<Slot>>
                           (link.lock()->Slots.begin() + si,
-                           link.lock()->Slots.begin() + sf + 1));
+                           link.lock()->Slots.begin() + si + RequiredSlots));
             }
         }
 
