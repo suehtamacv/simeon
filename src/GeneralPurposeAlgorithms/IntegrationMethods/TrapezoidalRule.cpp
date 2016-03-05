3144651c049d7268a53bd2f8536186e0ea9d5cbb
Matheus Cavalcante <matheus.cavalcante@ee.ufcg.edu.br>
2016-03-05 15:31:48 -0300

    Error was in TrapezoidalRule, after all.

--- src/GeneralPurposeAlgorithms/IntegrationMethods/TrapezoidalRule.cpp
+++ src/GeneralPurposeAlgorithms/IntegrationMethods/TrapezoidalRule.cpp
@@ -8,7 +8,13 @@ TrapezoidalRule::TrapezoidalRule(arma::mat &YPoints, double XRange)
 
 double TrapezoidalRule::calculate()
 {
-    double h = XRange / (YPoints.size() - 1);
-    return (h / 2.0) * (2 * arma::accu(YPoints) -
-                        *YPoints.begin() - *YPoints.end());
+    double Result, Sum = 0, h;
+    h = XRange / (YPoints.size() - 1);
+    for(unsigned int Cont = 1; Cont <= (YPoints.size() - 2); Cont++)
+        {
+        Sum += YPoints.at(Cont);
+        }
+
+    Result = (h / 2) * (YPoints.at(YPoints.size() - 1) + YPoints.at(0) + 2 * Sum);
+    return Result;
 }
