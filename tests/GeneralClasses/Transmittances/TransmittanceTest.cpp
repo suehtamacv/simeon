aca8d5afbb9d7ef0be3f66beb3a7f30737cb1425
lucas-of <lucas.figueiredo.pdf@gmail.com>
2016-06-26 18:13:26 -0300

    UnitTest: More Gaussian Passband and Stopband tests.

--- tests/GeneralClasses/Transmittances/TransmittanceTest.cpp
+++ tests/GeneralClasses/Transmittances/TransmittanceTest.cpp
@@ -24,7 +24,7 @@ TEST(TransmittanceTest, ConstantTransmittanceTest)
     double freqValue2 = 193.4E12 + 25E9;
     EXPECT_EQ(ConstTransmit.get_TransmittanceAt(freqValue1),
               ConstTransmit.get_TransmittanceAt(freqValue2)) <<
-                      "The frequency is being considered in the transmittance.";
+                      "The frequency is being considered in the constant transmittance.";
 }
 
 TEST(TransmittanceTest, GaussianPassbandFilterTest)
@@ -39,6 +39,50 @@ TEST(TransmittanceTest, GaussianPassbandFilterTest)
               filterOrder) << "The proper filter order is not being initialized.";
     EXPECT_EQ(GaussPBFilter.get_Gain(), Devices::SSS::SSSLoss)
             << "The proper gain is not being initialized.";
+
+    double freqRange = 25E9;
+    EXPECT_LT(GaussPBFilter.get_TransmittanceAt(GaussPBFilter.get_CenterFreq() - freqRange),
+              GaussPBFilter.get_TransmittanceAt(GaussPBFilter.get_CenterFreq())) <<
+                      "Gaussian passband filter function exhibiting irregular behavior.";
+    EXPECT_LT(GaussPBFilter.get_TransmittanceAt(GaussPBFilter.get_CenterFreq() + freqRange),
+              GaussPBFilter.get_TransmittanceAt(GaussPBFilter.get_CenterFreq())) <<
+                      "Gaussian passband filter function exhibiting irregular behavior.";
+
+    GaussianStopbandFilter GaussSBFilter(centerFreq, filterOrder, Devices::SSS::SSSLoss);
+    Gain GPBGain = GaussPBFilter.get_TransmittanceAt(centerFreq) - GaussPBFilter.get_Gain();
+    Gain GSBGain = GaussSBFilter.get_TransmittanceAt(centerFreq) - GaussSBFilter.get_Gain();
+    EXPECT_EQ(GPBGain.in_Linear(),
+              1 - GSBGain.in_Linear()) <<
+                      "Gaussian Passband function not complementary to Gaussian Stopband function.";
+}
+
+TEST(TransmittanceTest, GaussianStopbandFilterTest)
+{
+    constexpr static double centerFreq = 193.4E12;
+    unsigned int filterOrder = 1;
+    GaussianStopbandFilter GaussSBFilter(centerFreq, filterOrder, Devices::SSS::SSSLoss);
+
+    EXPECT_EQ(GaussSBFilter.get_CenterFreq(),
+              centerFreq) << "The proper frequency is not being initialized.";
+    EXPECT_EQ(GaussSBFilter.get_FilterOrder(),
+              filterOrder) << "The proper filter order is not being initialized.";
+    EXPECT_EQ(GaussSBFilter.get_Gain(), Devices::SSS::SSSLoss)
+            << "The proper gain is not being initialized.";
+
+    double freqRange = 25E9;
+    EXPECT_GT(GaussSBFilter.get_TransmittanceAt(GaussSBFilter.get_CenterFreq() - freqRange),
+              GaussSBFilter.get_TransmittanceAt(GaussSBFilter.get_CenterFreq())) <<
+                      "Gaussian stopband filter function exhibiting irregular behavior.";
+    EXPECT_GT(GaussSBFilter.get_TransmittanceAt(GaussSBFilter.get_CenterFreq() + freqRange),
+              GaussSBFilter.get_TransmittanceAt(GaussSBFilter.get_CenterFreq())) <<
+                      "Gaussian stopband filter function exhibiting irregular behavior.";
+
+    GaussianPassbandFilter GaussPBFilter(centerFreq, filterOrder, Devices::SSS::SSSLoss);
+    Gain GPBGain = GaussPBFilter.get_TransmittanceAt(centerFreq) - GaussPBFilter.get_Gain();
+    Gain GSBGain = GaussSBFilter.get_TransmittanceAt(centerFreq) - GaussSBFilter.get_Gain();
+    EXPECT_EQ(GPBGain.in_Linear(),
+              1 - GSBGain.in_Linear()) <<
+                      "Gaussian Stopband function not complementary to Gaussian Passband function.";
 }
 
 #endif
